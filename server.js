9f52a088542abc93691574e3d2a7eb664e324f76
sschwarz <schwarzs395@gmail.com>
2018-08-21 16:50:51 +0200

    Version 1.0.0

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const path = require('path')
 
 const items = require('./routes/api/items')
 
@@ -21,6 +22,16 @@ const port = process.env.PORT || 5000
 
 // Use routes
 app.use('/api/items', items)
+
+// Serve static assets (client build folder) 
+if(process.env.NODE_ENV === 'production') {
+        // Set static folder
+        app.use(express.static('client/build'))
+
+        app.get('*', (req, res) => {
+                res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+        })
+}
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
 
